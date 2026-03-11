62ba4a454b9f4c34a8d96d9749edb694e18f60c1
juh2 <juh2.dev@gmail.com>
2026-03-11 23:32:52 +0900

    Initial Commit

---
+++ jjanpot/jjanpotApp.swift
@@ -0,0 +1,17 @@
+//
+//  jjanpotApp.swift
+//  jjanpot
+//
+//  Created by 임주희 on 3/11/26.
+//
+
+import SwiftUI
+
+@main
+struct jjanpotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
